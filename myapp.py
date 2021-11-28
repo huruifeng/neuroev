f056abbb15add3d318c81a2d4279d471f282a0fa
RuifengHu <442490020@qq.com>
2021-11-28 16:36:06 -0500

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,83 @@
+import base64
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="NeuroEV",
+    # page_icon="favicon.ico",
+    layout = 'wide',
+    initial_sidebar_state="expanded",
+)
+############################################
+
+import interfaces.home
+import interfaces.expression
+import interfaces.cluster
+import interfaces.de
+
+
+## Functins / Style
+st.markdown(
+    '''
+        <style>
+             img.logo {
+                    margin: 0px auto;
+                    margin-top: -60px;
+                    margin-bottom: 25px;
+                    width: 60px;
+                }
+        </style>
+    ''',
+    unsafe_allow_html=True
+)
+
+def local_image(url):
+    file_ = open(url, "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    return data_url
+
+PAGES = {
+    "Home": interfaces.home,
+    "Expression Data": interfaces.expression,
+    "Gene Clusters": interfaces.cluster,
+    "DE Analysis": interfaces.de,
+}
+
+##================================================================
+def main():
+    ## Sidebar
+    col1, col2 = st.sidebar.columns([3,9])
+    with col1:
+        st.image('images/side_img.png', width=70)
+    with col2:
+        title_str='<span style="font-family:sans-serif; color:Black; font-size: 50px;">NeuroEV</span>'
+        st.markdown(title_str,unsafe_allow_html=True)
+
+    st.sidebar.title("Navigation")
+    select_page = st.sidebar.radio("Go to page", list(PAGES.keys()),key="page_radio")
+
+    page = PAGES[select_page]
+
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This app is used for exploring the gene expression ptterns in Human iPSC-derived Exosomes 
+        in Dopamine Neuron Differentiation.
+        """
+    )
+
+    page.main()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
